2a9bf685c820adc6394c664bdfd8cb18869c9cff
Jason Harris <13261827+jason-todd-harris@users.noreply.github.com>
2019-11-02 02:59:04 -0400

    csv2keychain-onowahoo

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ from setuptools import setup
 
 version = re.search(
     '^__version__\s*=\s*"(.*)"',
-    open('csv2keychain/__main__.py').read(),
+    open('csv2keychain-onowahoo/__main__.py').read(),
     re.M
 ).group(1)
 
 setup(
-    name="cmdline-csv2keychain",
-    packages=["csv2keychain"],
+    name="cmdline-csv2keychain-onowahoo",
+    packages=["csv2keychain-onowahoo"],
     entry_points={
-        "console_scripts": ['csv2keychain = csv2keychain.__main__:main']
+        "console_scripts": ['csv2keychain-onowahoo = csv2keychain-onowahoo.__main__:main']
     },
     version=version,
     description="Tool for adding exported credentials from Chrome to the macOS keychain.",
